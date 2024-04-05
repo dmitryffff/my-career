e62e3b636952bfef204c79461f524d06ceb73482
Dmitry Fominenkov <dmitry.fominenkov@mail.ru>
2024-04-06 00:53:43 +0300

    fixed prettier in esling

--- apps/users/src/dto/create-update.dto.ts
+++ apps/users/src/dto/create-update.dto.ts
@@ -1,9 +1,7 @@
 import z from 'zod'
 
 export const createUserDtoScheme = z
-  .object({
-    phoneNumber: z.string().regex(/^\d{10}$/gm),
-  })
+  .object({ phoneNumber: z.string().regex(/^\d{10}$/gm) })
   .strict()
 
 export type CreateUserDto = z.infer<typeof createUserDtoScheme>
