62ac4fbf256702db31fbe538b084f0167dd601d3
Dmitry Fominenkov <dmitry.fominenkov@mail.ru>
2024-04-06 16:50:30 +0300

    added changing the tsconfig file

--- add-package.ts
+++ add-package.ts
@@ -12,12 +12,16 @@ type PackageJson = {
 const settingsDevDeps = {
   '@repo/eslint-config': '*',
   '@repo/typescript-config': '*',
+  "eslint": "^8.56.7",
+  "@types/eslint": "^8.56.7",
 }
 
 const BUN_CREATE_SCRIPT = 'create'
 const REQUIRED_ARGS = {
   type: ['-t', '--type'], 
   name: ['-n', '--name'],
+  typescriptConfig: ['-tc', '--typescriptConfig'],
+
 } as const
 // [[type, ['-t', '--type']], [name, ['-n', '--name']]].forEach(([key, value]) => )
 const TYPE_VALUES = ['app', 'package', 'a', 'p'] as const
@@ -30,7 +34,14 @@ type Args = {
   typeValue: TypeValues;
   projectTypeDir: string;
   name: string;
+  typescriptConfig: string;
 };
+
+const getRepoProject = (projectName: string) => `@repo/${projectName}`
+
+const removeCommentsFromJson = (jsonString: string): string => {
+  return jsonString.replace(/\/\/.*|\/\*[^]*?\*\//g, '');
+}
 
 const checkRequiredArgs = (args: string[]) => {
   Object.entries(REQUIRED_ARGS).forEach(([key, value]) => {
@@ -63,6 +74,7 @@ const parseArgs = (args: string[]): Args => {
     typeValue: 'a',
     projectTypeDir: '',
     name: '',
+    typescriptConfig: '',
   };
 
   for (let i = 0; i < args.length; i++) {
@@ -91,13 +103,20 @@ const parseArgs = (args: string[]): Args => {
         }
         parsed.name = value;
         break;
+      case '-tc':
+      case '--typescriptConfig':
+        if (value === undefined) {
+          throw new Error(`-tc(--typescriptConfig) must be defined`);
+        }
+        parsed.typescriptConfig = value;
+        break
     }
     i++; // skip next
   }
   return parsed;
 }
 
-const changePackageJson = async (type: TypeValues, path: string) => {
+const changePackageJson = async (path: string, type: TypeValues) => {
   const file = Bun.file(`${path}/package.json`)
   const parsedPackageJson = await file.json() as PackageJson
   parsedPackageJson.devDependencies = {
@@ -106,16 +125,35 @@ const changePackageJson = async (type: TypeValues, path: string) => {
   }
 
   if (type === 'p' || type === 'package') {
-    parsedPackageJson.name = `@repo/${parsedPackageJson.name}`
+    parsedPackageJson.name = getRepoProject(parsedPackageJson.name)
     parsedPackageJson.exports = {}
   }
 
   await Bun.write(file, JSON.stringify(parsedPackageJson, null, 2))
 }
 
+const changeTsConfig = async (path: string, typescriptConfig: string) => {
+  const file = Bun.file(`${path}/tsconfig.json`)
+  let stringifyJsonWithComments = await file.text()
+  const extendsProp = `  "extends": "${getRepoProject('typescript-config')}/${typescriptConfig}.json",\n`
+  stringifyJsonWithComments = stringifyJsonWithComments.slice(0, 2) + extendsProp + stringifyJsonWithComments.slice(2)
+  await Bun.write(file, stringifyJsonWithComments)
+}
+
+const addEslintConfig = () => {
+
+}
+
 async function main(): Promise<void> {
   checkRequiredArgs(Bun.argv)
-  const { scriptRunner, template, typeValue, projectTypeDir, name } = parseArgs(Bun.argv);
+  const { 
+    scriptRunner,
+    template,
+    typeValue,
+    typescriptConfig,
+    projectTypeDir,
+    name,
+  } = parseArgs(Bun.argv);
   const path = `./${projectTypeDir}/${name}`;
 
   // Execute the init script in the target directory
@@ -125,7 +163,8 @@ async function main(): Promise<void> {
   Bun.spawnSync(initScript, { cwd: path })
 
   await Promise.all([
-    changePackageJson(typeValue, path),
+    changePackageJson(path, typeValue),
+    changeTsConfig(path, typescriptConfig),
   ])
 
   // Install dependencies
