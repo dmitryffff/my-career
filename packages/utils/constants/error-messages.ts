133498a9956d3feb0d70d9a826ab53123365acc0
Dmitry Fominenkov <dmitry.fominenkov@mail.ru>
2024-04-15 03:04:01 +0300

    added DOES_NOT_EXISTS, CANT_CRUD error messages

--- packages/utils/constants/error-messages.ts
+++ packages/utils/constants/error-messages.ts
@@ -1,8 +1,23 @@
 import { capitalize } from '../helpers'
 
+const CRUD_ACTIONS = {
+  create: 'создать',
+  read: 'получить',
+  update: 'обновить',
+  delete: 'удалить',
+} as const
+
 export const ERROR_MESSAGES = {
-  SERVER_ERROR: 'Ошибка сервера',
-  /** Returns a message that `some-entity-name` with `value-name` already exists */
+  SERVER_ERROR: 'Ошибка сервера.',
   EXISTS: (entityName: string, propName: string) =>
-    `${capitalize(entityName)} с таким значением поля '${propName}' уже существует`,
+    `${capitalize(entityName)} с таким значением поля '${propName}' уже существует.`,
+  DOES_NOT_EXISTS: (entityName: string, propName: string) =>
+    `${capitalize(entityName)} с таким значением поля '${propName}' не существует.`,
+  CANT_CRUD: (
+    action: keyof typeof CRUD_ACTIONS,
+    entityName: string,
+    propName: string,
+    info?: string,
+  ) =>
+    `Невозможно ${CRUD_ACTIONS[action]} запись типа ${entityName} с таким значением поля '${propName}.'${info === undefined ? '' : `\nПричина: ${info}.`}`,
 }
